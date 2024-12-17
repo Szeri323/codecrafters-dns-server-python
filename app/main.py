c71c216c7fbb46bd6138d222c5c2565ffb6e5252
Szeri <szeri323@gmail.com>
2024-12-17 17:21:55 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,25 +14,13 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
             
-            pid = 1234
-            qr = 1
-            opcode = 0
-            aa = 0
-            tc = 0
-            rd = 0
-            ra = 0
-            z = 0
-            rcode = 0
-            qdcount = 0
-            ancount = 0
-            nscount = 0
-            arcount = 0
-            dns_header = f"{pid}{qr}{opcode}{aa}{tc}{rd}{ra}{z}{rcode}{qdcount}{ancount}{nscount}{arcount}"
+            pid = b"\x04\xd2"
+            qr = b"\x80"
             
-
-            dns_message = f"{dns_header}"
             
-            response = bytes(dns_header, encoding="utf-8")
+            response = pid + qr + (b"\x00" * 9)
+            
+            print(list(response))
             
             print(f"buf: {buf}")
             print(f"source: {source}")
