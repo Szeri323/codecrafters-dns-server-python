d52d7e37046d1bc580203814db6645b778c49a05
Szeri <szeri323@gmail.com>
2024-12-17 19:31:09 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,11 +14,29 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
             
-            pid = b"\x04\xd2"
-            qr = b"\x80"
+            p_id = b"\x04\xd2" # Packet Identifier (ID) 16 bits
+            p_qr = b"\x81" # Query/Response Indicator (QR) 1 bit
+            p_opcode = b"\x80"
+            p_aa = b""
+            p_tc = b""
+            p_rd = b""
+            p_ra = b""
+            p_z = b""
+            p_rcode = b""
+            p_qdcount = b"\x01"
+            p_ancount = b""
+            p_nscount = b""
+            p_arcount = b""
             
+            header = p_id + p_qr + p_opcode + b"\x00" + p_qdcount + b"\x00" + b"\x00" + (b"\x00" * 4)
             
-            response = pid + qr + (b"\x00" * 9)
+            q_domain = b"\x0ccodecrafters\x02io\x00"
+            q_type = b"\x00\x01"
+            q_class = b"\x00\x01"
+            
+            questions = q_domain + q_type + q_class
+            
+            response = header + questions
             
             print(list(response))
             
